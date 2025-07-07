f89bf209401b9e19b97d1a122e84434cef5ebdbb
filip.tunguz <filipkralh11@gmail.com>
2025-07-07 14:10:07 +0200

    copy shadcn's cn util

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
-import {twMerge} from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 
 /**
  * Utility function to merge class names conditionally.
