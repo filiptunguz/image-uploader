f60d93385388e3b4c044492f81cfb89ce637b4ba
filip.tunguz <filipkralh11@gmail.com>
2025-07-07 16:33:36 +0200

    simple image previewer

---
+++ src/components/ImagePreviewer.tsx
@@ -0,0 +1,42 @@
+import { cn } from '../utils/utils.ts';
+
+type ImagePreviewerProps = {
+	cardClassName?: string;
+	containerClassName?: string;
+	files: File[];
+	gridColumns?: number;
+};
+
+export default function ImagePreviewer({ gridColumns = 4, ...props }: ImagePreviewerProps) {
+	const { files } = props;
+	if (files.length === 0) return null;
+
+	return (
+		<div
+			className={cn('grid gap-4 max-w-full p-4', props.containerClassName)}
+			style={{
+				gridTemplateColumns: `repeat(${gridColumns}, minmax(0, 1fr))`,
+			}}
+		>
+			{files.map((file, index) => {
+				const url = URL.createObjectURL(file);
+				return (
+					<div
+						key={index}
+						className={cn(
+							'flex-shrink-0 size-[200px] border border-gray-400 rounded-lg',
+							props.cardClassName,
+						)}
+					>
+						<img
+							src={url}
+							alt={`Preview ${index}`}
+							className="w-full h-full object-contain"
+							onLoad={() => URL.revokeObjectURL(url)} // Clean up memory
+						/>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
--- src/example/App.tsx
+++ src/example/App.tsx
@@ -1,9 +1,14 @@
 import ImageUploader from '../components/ImageUploader.tsx';
+import { useState } from 'react';
+import ImagePreviewer from '../components/ImagePreviewer.tsx';
 
 export default function App() {
+	const [files, setFiles] = useState<File[]>([]);
+
 	return (
-		<div className="flex justify-center items-center h-screen bg-white">
-			<ImageUploader onFileSelected={console.log} />
+		<div className="flex justify-center items-center h-screen bg-white flex-col gap-8">
+			<ImageUploader onFileSelected={setFiles} />
+			<ImagePreviewer files={files} />
 		</div>
 	);
 }
