8f202ffb386a20a0559dafae0708e26daa54c16e
filip.tunguz <filipkralh11@gmail.com>
2025-07-07 17:32:37 +0200

    remove image method

--- src/components/ImagePreviewer.tsx
+++ src/components/ImagePreviewer.tsx
@@ -1,4 +1,6 @@
 import { cn } from '../utils/utils.ts';
+import type { ReactNode } from 'react';
+import { CloseIcon } from '../icons/DefaultIcons.tsx';
 
 type ImagePreviewerProps = {
 	cardClassName?: string;
@@ -6,6 +8,8 @@ type ImagePreviewerProps = {
 	imageClassName?: string;
 	files: File[];
 	gridColumns?: number;
+	onRemove?: (file: File) => void;
+	removeIcon?: ReactNode;
 };
 
 export default function ImagePreviewer({ gridColumns = 4, ...props }: ImagePreviewerProps) {
@@ -27,8 +31,18 @@ export default function ImagePreviewer({ gridColumns = 4, ...props }: ImagePrevi
 						className={cn(
 							'flex-shrink-0 size-[200px] border border-gray-400 rounded-lg',
 							props.cardClassName,
+							props.onRemove && 'relative',
 						)}
 					>
+						{props.onRemove &&
+							(props.removeIcon ?? (
+								<div
+									className="absolute  top-1 right-1 text-gray-800 cursor-pointer hover:text-gray-400 transition-colors"
+									onClick={() => props.onRemove!(file)}
+								>
+									<CloseIcon />
+								</div>
+							))}
 						<img
 							src={url}
 							alt={`Preview ${index}`}
--- src/example/App.tsx
+++ src/example/App.tsx
@@ -3,12 +3,18 @@ import { useState } from 'react';
 import ImagePreviewer from '../components/ImagePreviewer.tsx';
 
 export default function App() {
+	// TODO: create wrapper component for ImageUploader and ImagePreviewer
+
 	const [files, setFiles] = useState<File[]>([]);
+
+	const handleRemove = (file: File) => {
+		setFiles((prevFiles) => prevFiles.filter((f) => f !== file));
+	};
 
 	return (
 		<div className="flex justify-center items-center h-screen bg-white flex-col gap-8">
 			<ImageUploader onFileSelected={setFiles} />
-			<ImagePreviewer files={files} />
+			<ImagePreviewer files={files} onRemove={handleRemove} />
 		</div>
 	);
 }
