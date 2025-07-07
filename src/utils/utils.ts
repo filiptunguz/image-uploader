209f77e3d800b63f6e2ca61d1e2eecb629ecf03d
filip.tunguz <filipkralh11@gmail.com>
2025-07-07 14:03:57 +0200

    cn util

---
+++ src/utils/utils.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from "clsx"
+import {twMerge} from "tailwind-merge";
+
+/**
+ * Utility function to merge class names conditionally.
+ * Originally from Shadcn UI (https://ui.shadcn.com/docs/installation/manual)
+ */
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs))
+}
